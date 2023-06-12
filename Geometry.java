c3d3a02de4feac4b971a8cb54daee68f6fdb9dd1
LexidaysicalIBAT <125698960+LexidaysicalIBAT@users.noreply.github.com>
2023-06-12 21:04:07 +0100

    Add files via upload

---
+++ Geometry.java
@@ -0,0 +1,45 @@
+package GeometryTest.java;
+
+public class Geometry {
+public static main
+
+
+        areaRect(float width, float height) {
+		width = 4;
+		height = 7;
+		int c;
+		
+		System.out.println("The result of width x height is: " +c);
+		
+}
+private double radius;
+				
+		
+		Geometry(float width, float height) {
+			width = 15;
+			height = 3;
+			int c;
+			
+			System.out.println("The result of width x height is: " +c);
+			
+		}
+			
+		
+		float volumeSphere(float radius) {
+			float volumeSphere = (float) ((7.0 / 12) * Math.PI * Math.pow(radius, 12));
+			
+		}
+		
+			float surfaceAreaSphere(float radius) {
+				float volumeSphere = (float) ((9.0 / 2) * Math.PI * Math.pow(radius, 2));
+				
+		}
+				
+			
+		},(
+			
+		}
+	}
+
+
+
